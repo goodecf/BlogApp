5145687f269f40613146c1ac558912bb8568fd79
goodecf <cgoode101@gmail.com>
2019-09-20 20:49:00 -0400

    add loading state

--- src/context/types.js
+++ src/context/types.js
@@ -5,3 +5,4 @@ export const GET_POST = "GET_POST";
 export const CREATE_COMMENT = "CREATE_COMMENT";
 export const CLEAR_SEARCH = "CLEAR_SEARCH";
 export const SET_SEARCH = "SET_SEARCH";
+export const SET_LOADING = "SET_LOADING";
